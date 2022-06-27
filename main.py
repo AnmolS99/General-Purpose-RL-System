897cbec185ac7bb63e1c4a51196e338fea1f4e38
Anmol Singh <Anmol_singh@live.no>
2022-06-27 20:14:32 +0200

    Tune parameters

--- main.py
+++ main.py
@@ -34,37 +34,37 @@ def run():
     tf.random.set_seed(seed)
 
     # Creating the different sim worlds
-    pbsw = PoleBalancingSimWorld()
-    num_pegs = 3
-    num_discs = 4
+    pbsw = PoleBalancingSimWorld(l=1, m_p=0.3, g=5, timestep=0.5)
+    num_pegs = 5
+    num_discs = 6
     tohsw = TowersOfHanoiSimWorld(num_pegs, num_discs)
-    gsw = GamblerSimWorld(0.4)
+    gsw = GamblerSimWorld(0.6)
 
     # UNCOMMENT THE RLSYSTEM (rls) YOU WANT TO RUN
 
     # Pole balancing with table-based critic
-    # rls = RLSystem(pbsw, 200, 300, False, None, 0.3, 0.3, 0.5, 0.5, 0.99, 0.99,
-    #                0.5, 0.04, True, 1)
+    rls = RLSystem(pbsw, 2, 300, False, None, 0.3, 0.3, 0.5, 0.5, 0.99, 0.99,
+                   0.5, 0.04, True, 1)
 
     # Pole balancing with ANN-based critic
-    rls = RLSystem(pbsw, 100, 300, True, (20, 16, 16, 1), 0.3, 0.3, 0.5, 0.5,
-                   0.99, 0.99, 0.5, 0.1, True, 1)
+    # rls = RLSystem(pbsw, 3, 300, True, (20, 20, 20, 40, 5, 1), 0.3, 0.3, 0.5,
+    #                0.5, 0.5, 0.5, 0.5, 0.1, True, 1)
 
     # Towers of Hanoi with table-based critic
-    # rls = RLSystem(tohsw, 300, 300, False, None, 0.3, 0.03, 0.5, 0.5, 0.99,
+    # rls = RLSystem(tohsw, 10, 300, False, None, 0.3, 0.03, 0.5, 0.5, 0.99,
     #                0.99, 0.9, 0.03, True, 1)
 
     # Towers of Hanoi with ANN-based critic
-    # rls = RLSystem(tohsw, 10, 300, True, (num_pegs * num_discs, 16, 16, 1),
-    #                0.2, 0.01, 0.5, 0.5, 0.99, 0.99, 0.9, 0.03, True, 1)
+    # rls = RLSystem(tohsw, 10, 300, True, (num_pegs * num_discs, 30, 30, 30, 1),
+    #                2, 0.1, 0.5, 0.5, 0.99, 0.99, 0.9, 0.03, True, 1)
 
     # The Gambler with table-based critic
-    # rls = RLSystem(gsw, 25000, 300, False, None, 0.5, 0.05, 0.5, 0.5, 1, 1,
-    #                0.9, 0.075, True, 1)
+    # rls = RLSystem(gsw, 100, 300, False, None, 0.5, 0.05, 0.5, 0.5, 1, 1, 0.9,
+    #                0.075, True, 1)
 
     # The Gambler with ANN-based critic
-    # rls = RLSystem(gsw, 1000, 300, True, (101, 10, 10, 1), 0.08, 0.01, 0.5,
-    #                0.5, 1, 1, 0.9, 0.075, True, 1)
+    # rls = RLSystem(gsw, 10, 300, True, (101, 5, 1), 0.08, 0.01, 0.5, 0.5, 1, 1,
+    #                0.9, 0.075, True, 1)
 
     rls.generic_actor_critic_algorithm()
 
